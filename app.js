ecb771450cf7bbeb11a1eecb94aeb336ce4aaeca
Alexalom99 <alexalom1999@gmail.com>
2025-03-28 10:05:37 +0100

    done

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // Buena suerte ! Espero que se te encianda la "bombilla"!
+
+let bulb = document.querySelector("#bulb");
+let on = false;
+
+bulb.addEventListener("click", function () {
+    if (!on) {
+        bulb.classList.remove("bulb-off");
+        bulb.classList.add("bulb-on");
+        on = true;
+    }
+    else {
+        bulb.classList.remove("bulb-on");
+        bulb.classList.add("bulb-off");
+        on = false;
+    }
+})
